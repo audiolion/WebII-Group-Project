9246d530cc9bc765ef1e776dcc5bef42e214646a
Ricky Catron <catron.ricky@gmail.com>
2016-03-14 14:19:21 -0400

    Moved user views to own file and added project proposal.

---
+++ Django_Files/home/user_views.py
@@ -0,0 +1,58 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .models import UserProfile
+from .forms import LoginForm, RegisterForm
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        try:
+            login(request, user)
+        except:
+            messages.error("Username or password is incorrect")
+            return redirect(request.META['HTTP_REFERER'])
+        return redirect("/")
+    else:
+        return render(request, "form.html", {
+            'form': LoginForm(),
+            "title": "Login"
+        })
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST or None)
+        if (form.is_valid()):
+            user = User.objects.create_user(username=form.cleaned_data.get('username'),
+                                            email=form.cleaned_data.get('email'),
+                                            password=form.cleaned_data.get('password'))
+            user.save()
+            new_user = authenticate(username=form.cleaned_data.get('username'),
+                                    password=form.cleaned_data.get('password'))
+            user_profile = UserProfile(user_id=new_user.id)
+            user_profile.save()
+            try:
+                login(request, new_user)
+            except:
+                messages.error("There has been an issue with your registration please try again.")
+                return redirect(request.META['HTTP_REFERER'])
+            return redirect("/")
+        else:
+            messages.error("There has been an issue with your registration please try again.")
+            return redirect(request.META['HTTP_REFERER'])
+    else:
+        return render(request, "form.html", {
+            'form': RegisterForm(),
+            "title": "Register"
+        })
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
--- Django_Files/myproject/urls.py
+++ Django_Files/myproject/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from home import views
+from home import views, user_views
 
 from . import settings
 
@@ -10,7 +10,7 @@ urlpatterns = [
                   url(r'^admin/', include(admin.site.urls)),
                   url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
                   url(r'^account/', include('django.contrib.auth.urls')),
-                  url(r'^login/$', views.login_view, name='login'),
-                  url(r'^register/$', views.register_view, name='register'),
-                  url(r'^logout/$', views.logout_view, name='logout'),
+                  url(r'^login/$', user_views.login_view, name='login'),
+                  url(r'^register/$', user_views.register_view, name='register'),
+                  url(r'^logout/$', user_views.logout_view, name='logout'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
