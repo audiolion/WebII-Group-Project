af630dce985c7ae12b7b51bf920fba8159b16e10
Ricky Catron <catron.ricky@gmail.com>
2016-04-13 20:41:04 -0400

    Mandated login page for users

--- Django_Files/home/views.py
+++ Django_Files/home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Lesson
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, "index.html")
 
 
+@login_required(login_url='/login/')
 def lessons(request, lesson_number=1):
     return render(request, "lessons.html", {
         "lessons": Lesson.objects.all().order_by("lesson_number"),
@@ -13,21 +15,26 @@ def lessons(request, lesson_number=1):
     })
 
 
+@login_required(login_url='/login/')
 def reference(request):
     return render(request, "reference.html")
 
 
+@login_required(login_url='/login/')
 def forum(request):
     return render(request, "forum.html")
 
 
+@login_required(login_url='/login/')
 def dashboard(request):
     return render(request, "dashboard.html")
 
 
+@login_required(login_url='/login/')
 def language_tools(request):
     return render(request, "language_tools.html")
 
 
+@login_required(login_url='/login/')
 def about_us(request):
     return render(request, "about_us.html")
